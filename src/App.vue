c0cbefa4719331a5b560245ca9420e31afe93c62
Reymon <rahidalgob@uce.edu.ec>
2024-01-11 19:46:28 -0500

    Examen1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Puntaje: {{ puntaje }}</h1>
+  <h1>intento: {{ intento }}</h1>
+  <div class="container">
+    <Game
+      ref="game1"
+      url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      @txt="actualizarPuntaje"
+    />
+    <Game
+      ref="game2"
+      url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg"
+      @txt="actualizarPuntaje"
+    />
+    <Game
+      ref="game3"
+      url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg"
+      @txt="actualizarPuntaje"
+    />
+  </div>
+  <button @click="jugar" :disabled="intento >= 5">{{ txtButton }}</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Game from "./components/Game.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Game,
+  },
+  data() {
+    return {
+      puntaje: 0,
+      intento: 0,
+      txtButton: "Jugar",
+    };
+  },
+  methods: {
+    async jugar() {
+      if (this.intento < 5) {
+        this.intento++;
+        await this.$refs.game1.consumirAPI();
+        await this.$refs.game2.consumirAPI();
+        await this.$refs.game3.consumirAPI();
+      } else {
+        this.txtButton = "Intentos Agotados";
+      }
+    },
+    actualizarPuntaje(txt) {
+      if (txt === "yes") {
+        this.puntaje += 1;
+      } else if (txt === "yes yes") {
+        this.puntaje += 2;
+      } else if (txt === "yes yes yes") {
+        this.puntaje += 5;
+      }
+    },
+  },
+};
 </script>
+F
 
 <style>
 #app {
@@ -22,5 +69,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container {
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  grid-template-columns: repeat(3, 1fr);
 }
 </style>
---
+++ src/components/Game.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <img :src="img ||url" />
+    <h2>{{ text }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    url: String,
+  },
+  data() {
+    return {
+      text: "xxxxx",
+      img: null,
+    };
+  },
+  methods: {
+    async consumirAPI() {
+      const { answer, image } = await fetch("https://yesno.wtf/api").then((r) =>
+        r.json()
+      );
+      console.log(answer);
+      console.log(image);
+      this.text = answer;
+      this.img = image;
+    },
+  },
+};
+</script>
+
+<style>
+img {
+  width: 200px;
+  height: 200px;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
